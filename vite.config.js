aa7b9516614d375ae050eac4b3c7e81fb014355a
Kevin110026 <110026@st.chsh.chc.edu.tw>
2025-03-08 01:14:59 +0800

    try to fix style.css path problem

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
-import monacoEditorEsmPlugin from 'vite-plugin-monaco-editor-esm'
+import { defineConfig } from "vite";
+import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [monacoEditorEsmPlugin()],
   server: {
-    host: '0.0.0.0',  // 讓 WSL 內部可訪問
+    host: "0.0.0.0", // 讓 WSL 內部可訪問
     port: 5173,
+  },
+  build: {
+    assetsInlineLimit: 0, // 確保 CSS 檔案不會被內嵌
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./style.css"; @import "./reset.css";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      $fonts: resolve("./src"),
+    },
   },
 });
