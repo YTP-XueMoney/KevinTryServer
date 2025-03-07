7d6e2e5be57527f88c3b2ce67c0fa376ce8bba13
Kevin110026 <110026@st.chsh.chc.edu.tw>
2025-03-08 01:17:52 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
 import { resolve } from "path";
+import "./style.css";
+import "./reset.css";
+
 
 export default defineConfig({
   plugins: [monacoEditorEsmPlugin()],
@@ -11,13 +14,13 @@ export default defineConfig({
   build: {
     assetsInlineLimit: 0, // 確保 CSS 檔案不會被內嵌
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./style.css"; @import "./reset.css";`,
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "./style.css"; @import "./reset.css";`,
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       $fonts: resolve("./src"),
